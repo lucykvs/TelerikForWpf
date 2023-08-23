9b94c0335c67ca0f818b819b29687b99e22887bb
Lucy Vincent-Smith <lucy.vincent-smith@teck.com>
2023-08-23 05:34:22 -0700

    updated theming control w accent colors

--- AutoCompleteBoxThemes/MainWindow.xaml.cs
+++ AutoCompleteBoxThemes/MainWindow.xaml.cs
@@ -12,6 +12,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Telerik.Windows.Controls;
+using Telerik.Windows.Controls.DataVisualization.Map.BingRest;
 
 namespace AutoCompleteBoxThemes {
     /// <summary>
@@ -25,36 +27,13 @@ namespace AutoCompleteBoxThemes {
             this.DataContext = new ViewModel();
         }
 
-        private void RadButton_Click(object sender, RoutedEventArgs e) {
-            var themeName = ((Telerik.Windows.Controls.RadButton)sender).Content;
-            
-            Application.Current.Resources.MergedDictionaries.Clear();
-
-            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
-            {
-                Source = new Uri("/Telerik.Windows.Themes." + themeName + ";component/Themes/System.Windows.xaml", UriKind.Relative)
-            });
-            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
-            {
-                Source = new Uri("/Telerik.Windows.Themes." + themeName + ";component/Themes/Telerik.Windows.Controls.Input.xaml", UriKind.Relative)
-            });
-            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
-            {
-                Source = new Uri("/Telerik.Windows.Themes." + themeName + ";component/Themes/Telerik.Windows.Controls.xaml", UriKind.Relative)
-            });
-            Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
-            {
-                Source = new Uri("/Telerik.Windows.Themes." + themeName + ";component/Themes/Telerik.Windows.Controls.GridView.xaml", UriKind.Relative)
-            });
-        }
-
-        private void ListBox_MouseLeftButtonUp(object sender, RoutedEventArgs e) {
+        private void RadioButton_Click(object sender, RoutedEventArgs e) {
             if (sender is null)
             {
                 return;
             }
 
-            ThemeInfo selectedTheme = (sender as System.Windows.Controls.ListBox).SelectedItem as ThemeInfo;
+            ThemeInfo selectedTheme = ((sender as System.Windows.Controls.RadioButton).Content) as ThemeInfo;
             string selectedThemeName = selectedTheme.ThemeName;
 
             if (selectedThemeName != this.CurrentTheme)
@@ -77,9 +56,70 @@ namespace AutoCompleteBoxThemes {
                 });
                 Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
                 {
+                    Source = new Uri("/Telerik.Windows.Themes." + selectedThemeName + ";component/Themes/Telerik.Windows.Controls.Navigation.xaml", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
                     Source = new Uri("/Telerik.Windows.Themes." + selectedThemeName + ";component/Themes/Telerik.Windows.Controls.GridView.xaml", UriKind.RelativeOrAbsolute)
                 });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.RichTextBoxUI.xaml", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.Spreadsheet.xaml", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Documents.xaml", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Cloud.Controls.xaml ", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.Chart.xaml ", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.ConversationalUI.xaml", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.Data.xaml ", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.DataVisualization.xaml", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.FixedDocumentViewers.xaml", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.FixedDocumentViewersUI.xaml ", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.ImageEditor.xaml ", UriKind.RelativeOrAbsolute)
+                });
+                Application.Current.Resources.MergedDictionaries.Add(new ResourceDictionary()
+                {
+                    Source = new Uri("/Telerik.Windows.Themes.Office_Black;component/Themes/Telerik.Windows.Controls.RibbonView.xaml", UriKind.RelativeOrAbsolute)
+                });
             }
+
+            //Office2019Palette.LoadPreset(Office2019Palette.ColorVariation.Gray);
+            //this.Background = new SolidColorBrush() { Color = Office2019Palette.Palette.BaseBackgroundColor };
+            //this.ThemesList.Background = this.Background;
+
+
+            //Windows11Palette.LoadPreset(Windows11Palette.ColorVariation.Dark);
+            //this.Background = new SolidColorBrush() { Color = Colors.Black };
+            //this.ThemesList.Background = this.Background;
         }
     }
 }
